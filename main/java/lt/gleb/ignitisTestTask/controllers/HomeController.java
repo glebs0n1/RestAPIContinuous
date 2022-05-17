99cea103412dcdd8899f1477ec0364267432b1de
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-17 07:39:30 +0300

    Update HomeController.java

--- main/java/lt/gleb/ignitisTestTask/controllers/HomeController.java
+++ main/java/lt/gleb/ignitisTestTask/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package lt.pusnis.ignitisTestTask.controllers;
+package lt.gleb.ignitisTestTask.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
