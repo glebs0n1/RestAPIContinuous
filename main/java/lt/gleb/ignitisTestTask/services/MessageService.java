90ae46934e4e250edef79296a17c40656fbfeee2
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-17 07:36:54 +0300

    Update MessageService.java

--- main/java/lt/gleb/ignitisTestTask/services/MessageService.java
+++ main/java/lt/gleb/ignitisTestTask/services/MessageService.java
@@ -1,4 +1,4 @@
-package lt.pusnis.ignitisTestTask.services;
+package lt.gleb.ignitisTestTask.services;
 
 import lt.pusnis.ignitisTestTask.models.MyMessage;
 import lt.pusnis.ignitisTestTask.repositories.MyMessagesRepository;
