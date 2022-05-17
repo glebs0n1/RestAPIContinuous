4225e6237c334d74f280edb188443aa457f0bf9c
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-17 10:41:06 +0300

    Update WebSecurityConfig.java

--- main/java/lt/gleb/TestTask/security/WebSecurityConfig.java
+++ main/java/lt/gleb/TestTask/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package lt.pusnis.ignitisTestTask.security;
+package lt.gleb.ignitisTestTask.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
