9e59bd262aa0dd19c7e9c32c1a44456f6afe3d9c
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-17 10:24:02 +0300

    Delete main/java/lt/gleb/ignitisTestTask directory

--- main/java/lt/gleb/ignitisTestTask/repositories/MyMessagesRepository.java
+++
@@ -1,8 +0,0 @@
-package lt.gleb.ignitisTestTask.repositories;
-
-import lt.pusnis.ignitisTestTask.models.MyMessage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MyMessagesRepository extends CrudRepository<MyMessage,Integer> {
-
-}
--- main/java/lt/gleb/ignitisTestTask/repositories/MyRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package lt.gleb.ignitisTestTask.repositories;
-
-import lt.pusnis.ignitisTestTask.models.MyRole;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MyRoleRepository extends CrudRepository<MyRole,Integer> {
-
-}
