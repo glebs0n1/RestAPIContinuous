e49bf79012c4d073fea3f35e0d1126e767b48b76
Gleb Son <57506521+glebs0n1@users.noreply.github.com>
2022-05-17 07:37:40 +0300

    Update MyMessagesRepository.java

--- main/java/lt/gleb/ignitisTestTask/repositories/MyMessagesRepository.java
+++ main/java/lt/gleb/ignitisTestTask/repositories/MyMessagesRepository.java
@@ -1,4 +1,4 @@
-package lt.pusnis.ignitisTestTask.repositories;
+package lt.gleb.ignitisTestTask.repositories;
 
 import lt.pusnis.ignitisTestTask.models.MyMessage;
 import org.springframework.data.repository.CrudRepository;
