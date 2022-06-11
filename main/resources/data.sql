10f3c8baaa573c9ca47018bb963601d1f4e38b01
Gleb Son <songleb81@gmail.com>
2022-06-11 09:39:53 +0300

    Update data.sql

--- main/resources/data.sql
+++ main/resources/data.sql
@@ -1,20 +1,7 @@
 INSERT INTO users (  user_name,  user_role, user_token) VALUES ('User1', 2, 'user1');
 INSERT INTO users (  user_name,  user_role, user_token) VALUES ('User2', 2, 'user2');
-INSERT INTO users (  user_name,  user_role, user_token) VALUES ('User3', 2, 'user3');
-INSERT INTO users (  user_name,  user_role, user_token) VALUES ('User4', 2, 'user4');
-INSERT INTO users (  user_name,  user_role, user_token) VALUES ('User5', 1, 'user5');
 
 INSERT INTO roles ( role_name ) VALUES ('admin');
 INSERT INTO roles ( role_name ) VALUES ('user');
 
 INSERT INTO messages ( message_author, message_text)  VALUES (1,'Message 1');
-INSERT INTO messages ( message_author, message_text)  VALUES (1,'Message 2');
-INSERT INTO messages ( message_author, message_text)  VALUES (1,'Message 3');
-INSERT INTO messages ( message_author, message_text)  VALUES (1,'Message 4');
-INSERT INTO messages ( message_author, message_text)  VALUES (2,'Message 1');
-INSERT INTO messages ( message_author, message_text)  VALUES (3,'Message 1');
-INSERT INTO messages ( message_author, message_text)  VALUES (4,'Message 1');
-INSERT INTO messages ( message_author, message_text)  VALUES (5,'Message 1');
-INSERT INTO messages ( message_author, message_text)  VALUES (5,'Message 2');
-INSERT INTO messages ( message_author, message_text)  VALUES (5,'Message 3');
-INSERT INTO messages ( message_author, message_text)  VALUES (5,'Message 4');
