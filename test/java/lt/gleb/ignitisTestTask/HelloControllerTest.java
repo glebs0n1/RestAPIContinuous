d8e241db4d4786ad397a9a737495daf77d9f5fa5
Gleb Son <songleb81@gmail.com>
2022-06-11 09:38:56 +0300

    Update HelloControllerTest.java

--- test/java/lt/gleb/ignitisTestTask/HelloControllerTest.java
+++ test/java/lt/gleb/ignitisTestTask/HelloControllerTest.java
@@ -16,14 +16,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
-
     @Autowired
     private MockMvc mvc;
-
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-
     }
 }
